cf68ecce9d86aafc402829e5574a6bfcf1755fa0
adamjpatterson <adam@farar.net>
2024-11-27 23:10:23 -0500

    Provide an example of making a built-in dataset available.

--- project/package/results/__init__.py
+++ project/package/results/__init__.py
@@ -1,2 +1,8 @@
 import pathlib
+import pickle
 RESULTS_PATH = pathlib.Path(__file__).parent
+
+iris = None
+if RESULTS_PATH.joinpath('iris.data.pkl').exists():
+    with open(RESULTS_PATH.joinpath("iris.data.pkl"), "rb") as f:
+        iris = pickle.load(f)
